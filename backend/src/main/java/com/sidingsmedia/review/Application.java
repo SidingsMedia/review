5b366c37d0fd574732e6197371d1514453a49153
Mattie Nickson <mnickson@sidingsmedia.com>
2025-06-13 12:42:09 +0100

    Added CORS handling
    Signed-off-by: Mattie Nickson <mnickson@sidingsmedia.com>

--- backend/src/main/java/com/sidingsmedia/review/Application.java
+++ backend/src/main/java/com/sidingsmedia/review/Application.java
@@ -3,16 +3,36 @@
 
 package com.sidingsmedia.review;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("com.sidingsmedia.review")
 public class Application {
 
+    @Value("${server.cors.allowed-origin-patterns}")
+    private List<String> corsAllowedOriginPatterns;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns(corsAllowedOriginPatterns.toArray(new String[] {}));
+            }
+        };
     }
 
 }
