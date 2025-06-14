efd51a463325cd8d669585bb5a7d9c08918b731d
Mattie Nickson <mnickson@sidingsmedia.com>
2025-06-14 17:39:54 +0100

    Moved events load API call to events filter
    Again to save some repetition.
    
    Signed-off-by: Mattie Nickson <mnickson@sidingsmedia.com>

--- frontend/src/components/input/EventsFilter.tsx
+++ frontend/src/components/input/EventsFilter.tsx
@@ -3,16 +3,24 @@
 
 import type { DateTime } from "luxon";
 
+import type { InputBasePropsSizeOverrides } from "@mui/material/InputBase";
+import type { OverridableStringUnion } from "@mui/types";
+
 import * as React from "react";
 
-import Button from "@mui/material/Button";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Stack from "@mui/material/Stack";
 import { renderTimeViewClock } from "@mui/x-date-pickers/timeViewRenderers";
 
-import type { Monitor } from "../../lib/api/responses";
+import { useNotifications } from "@toolpad/core";
+
+import type { Event, List, Monitor } from "../../lib/api/responses";
+import { ApiError } from "../../lib/api/ApiError";
 import MonitorAutoSelect from "../input/MonitorAutoSelect";
+import { NOTIFICATION_DISPLAY_TIME } from "../../lib/api/constants";
+import { useApi } from "../../context/ApiContext";
 
 export interface EventsFilterError {
   dateFrom?: string | null;
@@ -21,23 +29,91 @@ export interface EventsFilterError {
 }
 
 export interface EventsFilterProps {
-  loading?: boolean;
-  onSearch: (
-    dateFrom: DateTime | null,
-    dateTo: DateTime | null,
-    selectedMonitors: Monitor[] | null,
-  ) => EventsFilterError | undefined;
+  size?: OverridableStringUnion<
+    "small" | "medium",
+    InputBasePropsSizeOverrides
+  >;
+  onSearch?: (events: Event[], selectedMonitors: Monitor[]) => void;
 }
 
 export default function EventsFilter(
   props: EventsFilterProps,
 ): React.JSX.Element {
-  const [selectedMonitors, setSelectedMonitors] = React.useState<
-    Monitor[] | null
-  >([]);
+  const api = useApi();
+  const notifications = useNotifications();
+
+  const [monitors, setMonitors] = React.useState<Monitor[] | null>([]);
   const [dateFrom, setDateFrom] = React.useState<DateTime | null>(null);
   const [dateTo, setDateTo] = React.useState<DateTime | null>(null);
   const [error, setError] = React.useState<EventsFilterError | undefined>();
+
+  const [loading, setLoading] = React.useState(false);
+
+  function loadEvents(): void {
+    setLoading(true);
+
+    setError(undefined);
+    const error: EventsFilterError = {};
+
+    let hasError = false;
+    if (!dateFrom) {
+      error.dateFrom = "Please enter a start date";
+      hasError = true;
+    }
+
+    if (!dateTo) {
+      error.dateTo = "Please enter an end date";
+      hasError = true;
+    }
+
+    if (!monitors || monitors.length === 0) {
+      error.monitors = "Please select at least one monitor";
+      hasError = true;
+    }
+
+    if (hasError) {
+      setLoading(false);
+      setError(error);
+      return;
+    }
+
+    void (async () => {
+      // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+      let request = `event?after=${(dateFrom as DateTime).toISO({ includeOffset: false }) as string}&before=${(dateTo as DateTime).toISO({ includeOffset: false }) as string}`;
+      if (monitors !== null && monitors.length > 0) {
+        for (const monitor of monitors) {
+          request += `&monitor=${monitor.id.toString()}`;
+        }
+      }
+
+      let events: List<Event>;
+      try {
+        events = await api.get<List<Event>>(request);
+      } catch (e) {
+        if (e instanceof ApiError) {
+          notifications.show(e.error.message, {
+            severity: "error",
+            autoHideDuration: NOTIFICATION_DISPLAY_TIME,
+          });
+          setLoading(false);
+          return;
+        }
+
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        notifications.show(`Network Error: ${e}`, {
+          severity: "error",
+          autoHideDuration: NOTIFICATION_DISPLAY_TIME,
+        });
+        setLoading(false);
+        return;
+      }
+
+      if (props.onSearch) {
+        setLoading(false);
+        props.onSearch(events.results, monitors ?? []);
+      }
+    })();
+  }
 
   return (
     <Stack
@@ -49,7 +125,7 @@ export default function EventsFilter(
     >
       <DateTimePicker
         disableFuture
-        disabled={props.loading}
+        disabled={loading}
         label="From"
         viewRenderers={{
           hours: renderTimeViewClock,
@@ -69,12 +145,13 @@ export default function EventsFilter(
             required: true,
             helperText: error?.dateFrom,
             error: !!error?.dateFrom,
+            size: props.size,
           },
         }}
       />
       <DateTimePicker
         disableFuture
-        disabled={props.loading}
+        disabled={loading}
         label="To"
         viewRenderers={{
           hours: renderTimeViewClock,
@@ -94,31 +171,33 @@ export default function EventsFilter(
             required: true,
             helperText: error?.dateTo,
             error: !!error?.dateTo,
+            size: props.size,
           },
         }}
       />
 
       <MonitorAutoSelect
         multiple
-        disabled={props.loading}
-        value={selectedMonitors}
+        disabled={loading}
+        value={monitors}
         onChange={(_e, val) => {
-          setSelectedMonitors(val);
+          setMonitors(val);
         }}
         error={!!error?.monitors}
         helperText={error?.monitors}
+        size={props.size}
       />
 
-      <Button
-        variant="outlined"
-        sx={{ p: 1, width: "56px", height: "56px" }}
-        loading={props.loading}
+      <IconButton
+        // variant="outlined"
+        // sx={{ p: 1, width: "56px", height: "56px" }}
+        loading={loading}
         onClick={() => {
-          setError(props.onSearch(dateFrom, dateTo, selectedMonitors));
+          loadEvents();
         }}
       >
         <SearchIcon />
-      </Button>
+      </IconButton>
     </Stack>
   );
 }
--- frontend/src/components/tables/EventSearchToolbar.tsx
+++ frontend/src/components/tables/EventSearchToolbar.tsx
@@ -1,11 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Sidings Media <contact@sidingsmedia.com>
 // SPDX-License-Identifier: MIT
 
-import type { DateTime } from "luxon";
-
 import type { GridSlotProps } from "@mui/x-data-grid";
-
-import type { EventsFilterError } from "../input/EventsFilter";
 
 import * as React from "react";
 
@@ -14,99 +10,22 @@ import Divider from "@mui/material/Divider";
 import Tooltip from "@mui/material/Tooltip";
 import ViewColumnIcon from "@mui/icons-material/ViewColumn";
 
-import { useNotifications } from "@toolpad/core/useNotifications";
-
-import type { Event, List, Monitor } from "../../lib/api/responses";
-import { ApiError } from "../../lib/api/ApiError";
+import type { Event, Monitor } from "../../lib/api/responses";
 import EventsFilter from "../input/EventsFilter";
-import { NOTIFICATION_DISPLAY_TIME } from "../../lib/api/constants";
-import { useApi } from "../../context/ApiContext";
 
 export default function EventSearchToolbar(
   props: GridSlotProps["toolbar"] & {
     onDataLoad?: (events: Event[], monitors: Monitor[]) => void;
   },
-) {
-  const api = useApi();
-  const notifications = useNotifications();
-
-  const [loading, setLoading] = React.useState(false);
-
-  function loadEvents(
-    dateFrom: DateTime | null,
-    dateTo: DateTime | null,
-    monitors: Monitor[] | null,
-  ): EventsFilterError | undefined {
-    setLoading(true);
-
-    const error: EventsFilterError = {};
-
-    let hasError = false;
-    if (!dateFrom) {
-      error.dateFrom = "Please enter a start date";
-      hasError = true;
-    }
-
-    if (!dateTo) {
-      error.dateTo = "Please enter an end date";
-      hasError = true;
-    }
-
-    if (!monitors || monitors.length === 0) {
-      error.monitors = "Please select at least one monitor";
-      hasError = true;
-    }
-
-    if (hasError) {
-      setLoading(false);
-      return error;
-    }
-
-    void (async () => {
-      // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-      let request = `event?after=${(dateFrom as DateTime).toISO({ includeOffset: false }) as string}&before=${(dateTo as DateTime).toISO({ includeOffset: false }) as string}`;
-      if (monitors !== null && monitors.length > 0) {
-        for (const monitor of monitors) {
-          request += `&monitor=${monitor.id.toString()}`;
-        }
-      }
-
-      let events: List<Event>;
-      try {
-        events = await api.get<List<Event>>(request);
-      } catch (e) {
-        if (e instanceof ApiError) {
-          notifications.show(e.error.message, {
-            severity: "error",
-            autoHideDuration: NOTIFICATION_DISPLAY_TIME,
-          });
-          setLoading(false);
-          return;
-        }
-
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        notifications.show(`Network Error: ${e}`, {
-          severity: "error",
-          autoHideDuration: NOTIFICATION_DISPLAY_TIME,
-        });
-        setLoading(false);
-        return;
-      }
-
-      if (props.onDataLoad) {
-        setLoading(false);
-        props.onDataLoad(events.results, monitors ?? []);
-      }
-    })();
-  }
-
+): React.JSX.Element {
   return (
     <Toolbar style={{ marginTop: 2, marginBottom: 2 }}>
       <EventsFilter
-        loading={loading}
-        onSearch={(dateFrom, dateTo, monitors) =>
-          loadEvents(dateFrom, dateTo, monitors)
-        }
+        onSearch={(events, monitors) => {
+          if (props.onDataLoad) {
+            props.onDataLoad(events, monitors);
+          }
+        }}
       />
 
       <Divider
